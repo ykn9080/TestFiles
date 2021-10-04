6d1f6548a26cdb6433b8ff855bc4259abf149f0b
ykn9080 <ykn9080@empal.com>
2021-10-04 16:36:56 +0800

    sample data chg

--- src/content.js
+++ src/content.js
@@ -226,7 +226,7 @@ const sampledata1 = {
     { Year: "2013", Population: 316128839 },
   ],
 };
-const sampledata = {
+const sampledata2 = {
   key: "ext5",
   title: "Partition Vector",
   type: "table",
@@ -377,5 +377,76 @@ const sampledata = {
     { Partition: 2 },
     { Partition: 1 },
     { Partition: 2 },
+  ],
+};
+const sampledata = {
+  setting: {
+    column: [
+      {
+        title: "name",
+        titletext: "name",
+        origin: "name",
+        dataIndex: "name",
+        key: "name",
+        datatype: "string",
+      },
+      {
+        title: "chinese",
+        titletext: "chinese",
+        origin: "chinese",
+        dataIndex: "chinese",
+        key: "chinese",
+        datatype: "int",
+      },
+      {
+        title: "math",
+        titletext: "math",
+        origin: "math",
+        dataIndex: "math",
+        key: "math",
+        datatype: "int",
+      },
+      {
+        title: "english",
+        titletext: "english",
+        origin: "english",
+        dataIndex: "english",
+        key: "english",
+        datatype: "int",
+      },
+    ],
+    reset: false,
+    title: "Score",
+    size: "small",
+  },
+  dtlist: [
+    {
+      key: "1",
+      name: "John Brown",
+      chinese: 98,
+      math: 60,
+      english: 70,
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      chinese: 98,
+      math: 66,
+      english: 89,
+    },
+    {
+      key: "3",
+      name: "Joe Black",
+      chinese: 98,
+      math: 90,
+      english: 70,
+    },
+    {
+      key: "4",
+      name: "Jim Red",
+      chinese: 88,
+      math: 99,
+      english: 89,
+    },
   ],
 };
